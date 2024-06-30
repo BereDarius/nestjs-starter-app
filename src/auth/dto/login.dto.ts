a66332f8620cc728a71996dff3db7ea9df602c49
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2024-06-30 19:49:19 +0300

    chore: Trim whitespace from username in LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class LoginDto {
   @ApiProperty({ example: 'JohnDoe' })
   @IsString()
   @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
   username: string;
 
   @ApiProperty({ example: 'password' })
