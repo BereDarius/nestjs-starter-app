5c56fe38460a40fb847b6788d71a8cb966696522
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2024-07-01 19:49:27 +0300

    refactor: Update QueryUserDto with optional properties and examples

--- src/users/dto/query-user.dto.ts
+++ src/users/dto/query-user.dto.ts
@@ -1,47 +1,35 @@
-import {
-  IsEmail,
-  IsEnum,
-  IsOptional,
-  IsString,
-  IsStrongPassword,
-} from 'class-validator';
+import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { RoleEnum } from 'src/auth/enums/roles.enum';
 import { Transform } from 'class-transformer';
 
 export class QueryUserDto {
-  @ApiProperty()
+  @ApiProperty({ required: false, example: 'John' })
   @IsOptional()
   @IsString()
   @Transform(({ value }) => value.trim())
   first_name?: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: false, example: 'Doe' })
   @IsOptional()
   @IsString()
   @Transform(({ value }) => value.trim())
   last_name?: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: false, example: 'JohnDoe' })
   @IsOptional()
   @IsString()
   @Transform(({ value }) => value.trim())
   username?: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: false, example: 'john.doe@email.com' })
   @IsOptional()
   @IsString()
   @IsEmail()
   @Transform(({ value }) => value.trim().toLowerCase())
   email?: string;
 
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  @IsStrongPassword()
-  password?: string;
-
-  @ApiProperty()
+  @ApiProperty({ required: false, example: 'USER', enum: RoleEnum })
   @IsOptional()
   @IsEnum(RoleEnum)
   @Transform(({ value }) => value.trim().toUpperCase())
